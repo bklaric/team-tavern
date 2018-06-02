9ddaa802a8c67948904860ebc2ae155fb091c866
Branimir Klarić <branimir.klaric.bk@gmail.com>
2018-06-02 14:09:23 +0200

    Remove generic options

--- src/Main.purs
+++ src/Main.purs
@@ -3,8 +3,6 @@ module Main where
 import Prelude
 
 import Data.Either (Either(Left, Right))
-import Data.Foreign.Generic.Types (SumEncoding(..))
-import Data.Foreign.Generic.Types as Generic
 import Data.HTTP.Method (CustomMethod, Method)
 import Data.Maybe (Maybe(..))
 import Data.Monoid (mempty)
@@ -36,19 +34,7 @@ listenOptions = TcpListenOptions
     , exclusive: Nothing
     }
 
-teamTavernRoutes = (JunctionProxy :: JunctionProxy TeamTavernRoutes)
-
-genericOptions :: Generic.Options
-genericOptions =
-    { sumEncoding: TaggedObject
-        { tagFieldName: "tag"
-        , contentsFieldName: "data"
-        , constructorTagTransform: id
-        }
-    , unwrapSingleConstructors: true
-    , unwrapSingleArguments: true
-    , fieldTransform: id
-    }
+teamTavernRoutes = JunctionProxy :: JunctionProxy TeamTavernRoutes
 
 clientConfig :: Options ClientConfig
 clientConfig =
@@ -58,8 +44,8 @@ clientConfig =
     <> port := 5432
     <> database := "team_tavern"
 
-requestHandler :: Pool -> Client -> Either CustomMethod Method -> Url -> Body -> (Response -> Effect Unit) -> Effect Unit
-requestHandler pool client method url body respond =
+handleRequest :: Pool -> Client -> Either CustomMethod Method -> Url -> Body -> (Response -> Effect Unit) -> Effect Unit
+handleRequest pool client method url body respond =
     case junctionRouter' teamTavernRoutes method (pathSegments url) (queryPairs url) of
     Left _ -> respond { statusCode: 404, content: "404 Not Found" }
     Right routeValues -> routeValues # match
@@ -68,14 +54,14 @@ requestHandler pool client method url body respond =
         , registerPlayer: const $ handleRegister Local pool client body respond
         }
 
-invalidUrlHandler :: Pool -> Client -> Request -> (Response -> Effect Unit) -> Effect Unit
-invalidUrlHandler pool client { method, url, body } respond =
+handleInvalidUrl :: Pool -> Client -> Request -> (Response -> Effect Unit) -> Effect Unit
+handleInvalidUrl pool client { method, url, body } respond =
     case url of
-    Right url' -> requestHandler pool client method url' body respond
+    Right url' -> handleRequest pool client method url' body respond
     Left url' -> respond { statusCode: 400, content: "Couldn't parse url '" <> url' <> "'." }
 
 main :: Effect Unit
 main = do
     pool <- Pool.create mempty clientConfig
     client <- Postmark.create "d763b189-d006-4e4a-9d89-02212ccd87f5"
-    run_ listenOptions (invalidUrlHandler pool client)
+    run_ listenOptions (handleInvalidUrl pool client)
