00556fb573230d1e2647400dc2f612314616858b
Branimir Klarić <branimir.klaric.bk@gmail.com>
2018-06-09 19:02:01 +0200

    Move RegisterErrorModel into ErrorModel module

--- src/TeamTavern/Player/Register/Error.purs
+++ src/TeamTavern/Player/Register/Error.purs
@@ -3,15 +3,11 @@ module TeamTavern.Player.Register.Error where
 import Prelude
 
 import Control.Monad.Eff.Console (log)
-import Data.Array (fromFoldable)
 import Data.Newtype (unwrap)
-import Data.Variant (SProxy(..), Variant, inj, match)
+import Data.Variant (Variant, match)
 import Effect (Effect)
 import Error.Class (message, name)
 import Node.Errors.Class (code)
-import TeamTavern.Player.Credentials (Credentials)
-import TeamTavern.Player.Email (EmailError)
-import TeamTavern.Player.Nickname (NicknameError)
 import TeamTavern.Player.Register.Database (DatabaseError)
 import TeamTavern.Player.Register.Identifiers (ModelError, ValidationError)
 import TeamTavern.Player.Register.SendEmail (SendEmailError)
@@ -72,47 +68,3 @@ logError registerError = unsafeCoerce do
                 <> show error.status <> ", " <> show error.code <> ", "
                 <> error.message
         }
-
-type RegisterErrorModel = Variant
-    ( validation :: Array (Variant
-        ( email ∷ Array EmailError
-        , nickname ∷ Array NicknameError
-        ))
-    , emailTaken :: {}
-    , nicknameTaken :: {}
-    , sendEmail :: { credentials :: Credentials }
-    , other :: {}
-    )
-
-_validation = SProxy :: SProxy "validation"
-
-_email = SProxy :: SProxy "email"
-
-_nickname = SProxy :: SProxy "nickname"
-
-_emailTaken = SProxy :: SProxy "emailTaken"
-
-_nicknameTaken = SProxy :: SProxy "nicknameTaken"
-
-_sendEmail = SProxy :: SProxy "sendEmail"
-
-_other = SProxy :: SProxy "other"
-
-fromRegisterPlayerErrors :: RegisterError -> RegisterErrorModel
-fromRegisterPlayerErrors = match
-    { model: const $ inj _other {}
-    , validation: _.errors
-        >>> fromFoldable
-        >>> map (match
-            { email: fromFoldable >>> inj _email
-            , nickname: fromFoldable >>> inj _nickname
-            })
-        >>> inj _validation
-    , token: const $ inj _other {}
-    , database: match
-        { emailTaken: const $ inj _emailTaken {}
-        , nicknameTaken: const $ inj _nicknameTaken {}
-        , other: const $ inj _other {}
-        }
-    , sendEmail: _.credentials >>> { credentials: _ } >>> inj _sendEmail
-    }
---
+++ src/TeamTavern/Player/Register/ErrorModel.purs
@@ -0,0 +1,54 @@
+module TeamTavern.Player.Register.ErrorModel where
+
+import Prelude
+
+import Data.Array (fromFoldable)
+import Data.Variant (SProxy(..), Variant, inj, match)
+import TeamTavern.Player.Credentials (Credentials)
+import TeamTavern.Player.Email (EmailError)
+import TeamTavern.Player.Nickname (NicknameError)
+import TeamTavern.Player.Register.Error (RegisterError)
+
+type RegisterErrorModel = Variant
+    ( validation :: Array (Variant
+        ( email ∷ Array EmailError
+        , nickname ∷ Array NicknameError
+        ))
+    , emailTaken :: {}
+    , nicknameTaken :: {}
+    , sendEmail :: { credentials :: Credentials }
+    , other :: {}
+    )
+
+_validation = SProxy :: SProxy "validation"
+
+_email = SProxy :: SProxy "email"
+
+_nickname = SProxy :: SProxy "nickname"
+
+_emailTaken = SProxy :: SProxy "emailTaken"
+
+_nicknameTaken = SProxy :: SProxy "nicknameTaken"
+
+_sendEmail = SProxy :: SProxy "sendEmail"
+
+_other = SProxy :: SProxy "other"
+
+fromRegisterPlayerErrors :: RegisterError -> RegisterErrorModel
+fromRegisterPlayerErrors = match
+    { model: const $ inj _other {}
+    , validation: _.errors
+        >>> fromFoldable
+        >>> map (match
+            { email: fromFoldable >>> inj _email
+            , nickname: fromFoldable >>> inj _nickname
+            })
+        >>> inj _validation
+    , token: const $ inj _other {}
+    , database: match
+        { emailTaken: const $ inj _emailTaken {}
+        , nicknameTaken: const $ inj _nicknameTaken {}
+        , other: const $ inj _other {}
+        }
+    , sendEmail: _.credentials >>> { credentials: _ } >>> inj _sendEmail
+    }
