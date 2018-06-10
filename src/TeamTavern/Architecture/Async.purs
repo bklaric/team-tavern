758e7a2dffddd5562ad5e2c8aa2edcce2b50c317
Branimir Klarić <branimir.klaric.bk@gmail.com>
2018-06-10 16:00:17 +0200

    Add examineErrorWith' to Architecture.Async

--- src/TeamTavern/Architecture/Async.purs
+++ src/TeamTavern/Architecture/Async.purs
@@ -5,7 +5,7 @@ import Prelude
 import Async (Async(..), runAsync)
 import Control.Monad.Cont (ContT(..))
 import Control.Monad.Except (ExceptT(..), withExceptT)
-import Data.Either (Either(..))
+import Data.Either (Either(..), either)
 import Data.Symbol (class IsSymbol, SProxy)
 import Data.Variant (Variant, inj)
 import Effect (Effect)
@@ -27,5 +27,18 @@ examineErrorWith logger async = Async $ ExceptT $ ContT $ \callback ->
         case _ of
         Left error -> do
             logger error
+            callback $ Left error
+        Right result -> callback $ Right result
+
+examineErrorWith'
+    :: forall left right
+    .  (left -> (forall voidLeft. Async voidLeft Unit))
+    -> Async left right
+    -> Async left right
+examineErrorWith' logger async = Async $ ExceptT $ ContT $ \callback ->
+    runAsync async
+        case _ of
+        Left error -> do
+            runAsync (logger error) (either absurd pure)
             callback $ Left error
         Right result -> callback $ Right result
