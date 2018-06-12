7cacc7259ea4f9c714b99a07ee6e1527b3d17a7b
Branimir Klarić <branimir.klaric.bk@gmail.com>
2018-06-12 18:54:27 +0200

    Add headers and cookies to Perun.Request

--- src/Perun/Request.purs
+++ src/Perun/Request.purs
@@ -2,9 +2,19 @@ module Perun.Request where
 
 import Prelude
 
+import Data.Array (mapMaybe)
 import Data.Bifunctor (lmap)
-import Data.Either (Either(..))
+import Data.Either (Either)
 import Data.HTTP.Method (CustomMethod, Method, fromString)
+import Data.Map (Map, fromFoldable)
+import Data.Maybe (Maybe(..))
+import Data.StrMap (lookup, toUnfoldable)
+import Data.String (Pattern(..), split)
+import Data.Traversable (traverse)
+import Data.Tuple (Tuple(..))
+import Either (swap)
+import Foreign (readStringMaybe)
+import Node.Http.IncomingMessage (headers)
 import Node.Http.Server.Request (method, url)
 import Node.Http.Server.Request as Node
 import Perun.Request.Body (Body, fromRequest)
@@ -13,14 +23,45 @@ import Perun.Url (Url, parseUrl)
 type Request =
     { method :: Either CustomMethod Method
     , url :: Either String Url
+    , headers :: Map String String
+    , cookies :: Maybe (Map String String)
     , body :: Body
     }
 
+readMethod :: Node.Request -> Either CustomMethod Method
+readMethod request = request # method # fromString # swap
+
+readUrl :: Node.Request -> Either String Url
+readUrl request = request # url # parseUrl # lmap (const $ url request)
+
+readHeaders :: Node.Request -> Map String String
+readHeaders request =
+    headers request
+    # toUnfoldable
+    # mapMaybe (traverse readStringMaybe)
+    # fromFoldable
+
+parseCookies :: String -> Maybe (Array (Tuple String String))
+parseCookies string =
+    split (Pattern "; ") string
+    <#> split (Pattern "=")
+    # traverse case _ of
+        [name, value] -> Just $ Tuple name value
+        _ -> Nothing
+
+readCookies :: Node.Request -> Maybe (Map String String)
+readCookies request =
+    headers request
+    # lookup "cookie"
+    >>= (readStringMaybe)
+    >>= parseCookies
+    <#> fromFoldable
+
 readRequest :: Node.Request -> Request
 readRequest request =
-    { method: request # method # fromString # case _ of
-        Left m -> Right m
-        Right cm -> Left cm
-    , url: request # url # parseUrl # lmap (const $ url request)
+    { method: readMethod request
+    , url: readUrl request
+    , headers: readHeaders request
+    , cookies: readCookies request
     , body: fromRequest request
     }
