338ecda413775fbcc152eaf5f95dce680db7d887
Branimir Klarić <branimir.klaric.bk@gmail.com>
2018-07-14 11:52:01 +0200

    Rename type variable computation to run

--- src/TeamTavern/Player/Register.purs
+++ src/TeamTavern/Player/Register.purs
@@ -20,34 +20,31 @@ derive instance functorRegisterF :: Functor RegisterF
 
 _register = SProxy :: SProxy "register"
 
-ensureNotSignedIn :: forall computations.
-    Run (register :: FProxy RegisterF | computations) Unit
+ensureNotSignedIn :: forall run. Run (register :: FProxy RegisterF | run) Unit
 ensureNotSignedIn = lift _register (EnsureNotSignedIn unit)
 
-readIdentifiers :: forall computations.
-    Run (register :: FProxy RegisterF | computations) IdentifiersModel
+readIdentifiers :: forall run.
+    Run (register :: FProxy RegisterF | run) IdentifiersModel
 readIdentifiers = lift _register (ReadIdentifiers id)
 
-validateIdentifiers
-    :: forall computations
-    .  IdentifiersModel
-    -> Run (register :: FProxy RegisterF | computations) Identifiers
+validateIdentifiers :: forall run.
+    IdentifiersModel -> Run (register :: FProxy RegisterF | run) Identifiers
 validateIdentifiers model = lift _register (ValidateIdentifiers model id)
 
-generateToken :: forall computations.
-    Identifiers -> Run (register :: FProxy RegisterF | computations) Token
+generateToken :: forall run.
+    Identifiers -> Run (register :: FProxy RegisterF | run) Token
 generateToken identifiers = lift _register (GenerateToken identifiers id)
 
-addPlayer :: forall computations.
-    Credentials -> Run (register :: FProxy RegisterF | computations) Unit
+addPlayer :: forall run.
+    Credentials -> Run (register :: FProxy RegisterF | run) Unit
 addPlayer credentials = lift _register (AddPlayer credentials unit)
 
-sendEmail :: forall computations.
-    Credentials -> Run (register :: FProxy RegisterF | computations) Unit
+sendEmail :: forall run.
+    Credentials -> Run (register :: FProxy RegisterF | run) Unit
 sendEmail credentials = lift _register (SendEmail credentials unit)
 
-register :: forall computations.
-    Run (register :: FProxy RegisterF | computations) Credentials
+register :: forall run.
+    Run (register :: FProxy RegisterF | run) Credentials
 register = do
     ensureNotSignedIn
     model <- readIdentifiers
