de45c0a23b2861a47cde93b55dc2341d01cc9271
Branimir Klarić <bklaric@pop-os.localdomain>
2018-08-25 15:15:57 +0200

    Add sign in links to welcome and code sent screens

--- src/TeamTavern/Client/Main.purs
+++ src/TeamTavern/Client/Main.purs
@@ -11,6 +11,8 @@ import Halogen as H
 import Halogen.HTML as HH
 import Simple.JSON (read)
 import TeamTavern.Client.Code as Code
+import TeamTavern.Client.Components.NavigationAnchor (navigationAnchor)
+import TeamTavern.Client.Components.NavigationAnchor as NavigationAnchor
 import TeamTavern.Client.Register as Register
 import TeamTavern.Client.Script.Cookie (PlayerInfo, getPlayerInfo)
 import TeamTavern.Client.SignIn as SignIn
@@ -33,6 +35,7 @@ type ChildSlots =
   , signIn :: SignIn.Slot Unit
   , register :: Register.Slot Unit
   , code :: Code.Slot Unit
+  , signInAnchor :: NavigationAnchor.Slot Unit
   )
 
 _topBar = SProxy :: SProxy "topBar"
@@ -43,24 +46,26 @@ _register = SProxy :: SProxy "register"
 
 _code = SProxy :: SProxy "code"
 
+_signInAnchor = SProxy :: SProxy "signInAnchor"
+
 type Message = Void
 
 render :: forall void. State -> H.ComponentHTML Query ChildSlots (A.Async void)
 render (Home playerInfo) = HH.slot _topBar unit topBar playerInfo absurd
 render SignIn = HH.slot _signIn unit SignIn.signIn unit absurd
 render Register = HH.slot _register unit Register.register unit absurd
-render (Welcome { email, nickname, emailSent }) = HH.p_
-    [ HH.text $ "Welcome to TeamTavern, " <> nickname <> "!"
-    , HH.br_
-    , if emailSent
+render (Welcome { email, nickname, emailSent }) = HH.div_
+    [ HH.p_ [ HH.text $ "Welcome to TeamTavern, " <> nickname <> "!" ]
+    , HH.p_ [ if emailSent
         then HH.text $ "Registration email with your sign in code has been sent to " <> email
-        else HH.text $ "Registration email has NOT been sent to " <> email
+        else HH.text $ "Registration email has NOT been sent to " <> email ]
+    , HH.p_ [ HH.slot _signInAnchor unit navigationAnchor { path: "/signin", text: "Sign in" } absurd ]
     ]
 render Code = HH.slot _code unit Code.code unit absurd
-render (CodeSent { email, nickname }) = HH.p_
-    [ HH.text $ "Hello, " <> nickname <> "!"
-    , HH.br_
-    , HH.text $ "An email with your sign in code has been sent to " <> email
+render (CodeSent { email, nickname }) = HH.div_
+    [ HH.p_ [ HH.text $ "Hello, " <> nickname <> "!" ]
+    , HH.p_ [ HH.text $ "An email with your sign in code has been sent to " <> email ]
+    , HH.p_ [ HH.slot _signInAnchor unit navigationAnchor { path: "/signin", text: "Sign in" } absurd ]
     ]
 render NotFound = HH.p_ [ HH.text "You're fucken lost, mate." ]
 
