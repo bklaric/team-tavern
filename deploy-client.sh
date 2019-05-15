cae384e3265c1e440aa15ccf1d4d53119e878c4e
Branimir Klarić <branimir.klaric.bk@gmail.com>
2019-05-15 23:10:27 +0200

    Add empty robots.txt

--- deploy-client.sh
+++ deploy-client.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm -rf dist-client
 mkdir dist-client
+cp robots.txt dist-client/robots.txt
 cp index.html dist-client/index.html
 cp src/TeamTavern/Client/Favicons/* dist-client/
 sass src/TeamTavern/Client/Style/Main.scss \
