bfc237f86055283ae5abe077a3becffdad6d810e
Branimir Klarić <branimir.klaric.bk@gmail.com>
2019-10-16 20:29:00 +0200

    Fix registration email text

--- src/TeamTavern/Server/Player/Register/SendEmail.purs
+++ src/TeamTavern/Server/Player/Register/SendEmail.purs
@@ -39,7 +39,7 @@ message { email, nickname, nonce } =
     , subject: toNullable $ Just "TeamTavern registration"
     , htmlBody: toNullable $ Just $
         "Hi " <> unwrap nickname <> ",<br /><br />"
-        <> "Thank you for registering to TeamTavern.Server. "
+        <> "Thank you for registering to TeamTavern. "
         <> "Please open the link below to sign in and verify your email address:<br /><br />"
         <> "<a href=\"https://www.teamtavern.net/signin?nonce=" <> unwrap nonce <> "\">https://www.teamtavern.net/signin?nonce=" <> unwrap nonce <> "</a><br /><br />"
         <> "Should you have any questions or feedback, please contact <a href=\"mailto:admin@teamtavern.net\">admin@teamtavern.net</a>. Thank you for your time.<br /><br />"
