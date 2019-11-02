9b8ed0068994f1b69762c41fe1db2b2d832a6fcd
Branimir Klarić <branimir.klaric.bk@gmail.com>
2019-11-02 13:54:23 +0100

    Check both email and nickname constraint errors

--- src/TeamTavern/Server/Player/Register/AddPlayer.purs
+++ src/TeamTavern/Server/Player/Register/AddPlayer.purs
@@ -56,8 +56,12 @@ addPlayer pool model @ { email, nickname, nonce } =
     # execute queryString (queryParameters model)
     # lmap (\error ->
         case code error == unique_violation of
+        true | constraint error == Just "player_email_key" ->
+            inj (SProxy :: SProxy "emailTaken") { email, error }
         true | constraint error == Just "player_lower_email_key" ->
             inj (SProxy :: SProxy "emailTaken") { email, error }
+        true | constraint error == Just "player_nickname_key" ->
+            inj (SProxy :: SProxy "nicknameTaken") { nickname, error }
         true | constraint error == Just "player_lower_nickname_key" ->
             inj (SProxy :: SProxy "nicknameTaken") { nickname, error }
         _ -> inj (SProxy :: SProxy "databaseError") error )
