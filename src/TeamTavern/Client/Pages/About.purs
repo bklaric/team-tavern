ef1c68fdea8e2a1245dbda9679a283972308f609
Branimir Klarić <branimir.klaric.bk@gmail.com>
2019-11-30 16:35:37 +0100

    Change meta tags on about page

--- src/TeamTavern/Client/Pages/About.purs
+++ src/TeamTavern/Client/Pages/About.purs
@@ -2,31 +2,35 @@ module TeamTavern.Client.Pages.About (Slot, about) where
 
 import Prelude
 
-import Async (Async)
 import Data.Const (Const)
 import Data.Maybe (Maybe(..))
 import Data.Symbol (SProxy(..))
+import Effect.Class (class MonadEffect)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
 import TeamTavern.Client.Components.NavigationAnchor (navigationAnchor)
-import TeamTavern.Client.Components.RegisterForm as RegisterForm
-import TeamTavern.Client.Home.CallToAction (callToAction)
-import TeamTavern.Client.Home.Games (games)
-import TeamTavern.Client.Home.Games as Games
-import TeamTavern.Client.Script.Cookie (PlayerInfo, getPlayerInfo)
+import TeamTavern.Client.Components.NavigationAnchor as NavigationAnchor
 import TeamTavern.Client.Script.Meta (setMetaDescription, setMetaTitle, setMetaUrl)
 import TeamTavern.Client.Snippets.SocialMediaUrls (discordUrl, redditUrl, steamUrl, twitterUrl)
 
+data Action = Init
+
 type Slot = H.Slot (Const Void) Void Unit
 
+type ChildSlots =
+    ( dotaAnchor :: NavigationAnchor.Slot Unit
+    , emailAnchor :: NavigationAnchor.Slot Unit
+    )
+render :: forall monad. MonadEffect monad =>
+    HH.ComponentHTML Action ChildSlots monad
 render = HH.div_
     [ HH.h1 [ HP.class_ $ HH.ClassName "about-title" ] [ HH.text "About TeamTavern" ]
     , HH.h2 [ HP.class_ $ HH.ClassName "about-heading" ] [ HH.text "What is TeamTavern?" ]
     , HH.p_
         [ HH.text $ "TeamTavern is an online platform for finding esports teammates. "
             <> "It aims to provide players of various online multiplayer games "
-            <> "with a common meeting point where they can find each other and form teams. "
+            <> "with a common meeting place where they can find each other and form teams. "
             <> "It currently features "
         , navigationAnchor (SProxy :: SProxy "dotaAnchor")
             { path: "/games/dota2/", content: HH.text "Dota 2" }
@@ -57,10 +61,24 @@ render = HH.div_
         [ HH.text "Come find out about the latest updates, give feedback, ask questions, find teammates or just say hi!" ]
     ]
 
+handleAction :: forall monad. MonadEffect monad => Action -> monad Unit
+handleAction Init = do
+    H.liftEffect do
+        setMetaTitle "About | TeamTavern"
+        setMetaDescription "TeamTavern is an online platform for finding esports teammates."
+        setMetaUrl
+
+component :: forall monad output input query. MonadEffect monad =>
+    H.Component HH.HTML query input output monad
 component = H.mkComponent
     { initialState: const unit
     , render: const render
     , eval: H.mkEval $ H.defaultEval
+        { handleAction = handleAction
+        , initialize = Just Init
+        }
     }
 
+about :: forall action monad children. MonadEffect monad =>
+    HH.ComponentHTML action (about :: Slot | children) monad
 about = HH.slot (SProxy :: SProxy "about") unit component unit absurd
