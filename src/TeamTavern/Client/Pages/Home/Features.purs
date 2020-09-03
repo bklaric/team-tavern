dd18b162523fea35a720fe82b0889b7c240d8957
Branimir Klarić <branimir.klaric.bk@gmail.com>
2020-09-03 22:01:58 +0200

    Add icons to reasons and features

--- src/TeamTavern/Client/Pages/Home/Features.purs
+++ src/TeamTavern/Client/Pages/Home/Features.purs
@@ -11,28 +11,36 @@ features =
     [ HH.h2 [ HP.class_ $ HH.ClassName "features-heading" ]
         [ HH.text "Amazing features" ]
     , HH.h3 [ HP.class_ $ HH.ClassName "features-feature-title" ]
-        [ HH.text "For players" ]
+        [ HH.i [ HP.class_ $ HH.ClassName "fas fa-user feature-icon" ] []
+        , HH.text "For players"
+        ]
     , HH.p  [ HP.class_ $ HH.ClassName "features-feature-description" ]
         [ HH.text """Are you a single player looking for a team?
         Create your player profile and let everyone know you're looking
         to team up. Find and join existing teams looking to recruit new
         members.""" ]
     , HH.h3 [ HP.class_ $ HH.ClassName "features-feature-title" ]
-        [ HH.text "For teams" ]
+        [ HH.i [ HP.class_ $ HH.ClassName "fas fa-users feature-icon" ] []
+        , HH.text "For teams"
+        ]
     , HH.p  [ HP.class_ $ HH.ClassName "features-feature-description" ]
         [ HH.text """Do you want to build a team or expand your existing
         online community? Create your team profile and let prospecting players
         know you're looking for new members. Find and invite like-minded players
         of matching skills and ambitions.""" ]
     , HH.h3 [ HP.class_ $ HH.ClassName "features-feature-title" ]
-        [ HH.text "Detailed profiles" ]
+        [ HH.i [ HP.class_ $ HH.ClassName "fas fa-address-card feature-icon" ] []
+        , HH.text "Detailed profiles"
+        ]
     , HH.p  [ HP.class_ $ HH.ClassName "features-feature-description" ]
         [ HH.text """Describe yourself or your team in great detail. Fill out
         personal details such as age, location, languages and mic usage as well
         as game specific details. Filter existing players and teams on all
         details to get the most relevant results.""" ]
     , HH.h3 [ HP.class_ $ HH.ClassName "features-feature-title" ]
-        [ HH.text "Built-in messaging" ]
+        [ HH.i [ HP.class_ $ HH.ClassName "fas fa-envelope feature-icon" ] []
+        , HH.text "Built-in messaging"
+        ]
     , HH.p  [ HP.class_ $ HH.ClassName "features-feature-description" ]
         [ HH.text """Found an interesting player or team you want to play with?
         Message them directly on TeamTavern and team up in-game. Receive an
--- src/TeamTavern/Client/Pages/Home/Why.purs
+++ src/TeamTavern/Client/Pages/Home/Why.purs
@@ -11,12 +11,16 @@ why =
     [ HH.h2 [ HP.class_ $ HH.ClassName "why-heading" ]
         [ HH.text "Why TeamTavern" ]
     , HH.h3 [ HP.class_ $ HH.ClassName "why-reason-title" ]
-        [ HH.text "Unparalleled team finding platform" ]
+        [ HH.i [ HP.class_ $ HH.ClassName "fas fa-laptop why-icon" ] []
+        , HH.text "Unparalleled team finding platform"
+        ]
     , HH.p  [ HP.class_ $ HH.ClassName "why-reason-description" ]
         [ HH.text """We aim to make TeamTavern the most versatile team finding
         platform and we're always looking for ways to improve.""" ]
     , HH.h3 [ HP.class_ $ HH.ClassName "why-reason-title" ]
-        [ HH.text "Responsive support and development" ]
+        [ HH.i [ HP.class_ $ HH.ClassName "fas fa-comments why-icon" ] []
+        , HH.text "Responsive support and development"
+        ]
     , HH.p  [ HP.class_ $ HH.ClassName "why-reason-description" ]
         [ HH.text """Have a suggestion? We always listen to feedback to ensure
         we build the platform players want.""" ]
