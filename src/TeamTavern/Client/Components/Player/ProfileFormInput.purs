f7628349bcd099abfb37b9c195d4a8d77c1fa8c9
Branimir Klarić <branimir.klaric.bk@gmail.com>
2020-10-25 01:00:14 +0200

    Fix create player profile input

--- src/TeamTavern/Client/Components/Player/ProfileFormInput.purs
+++ src/TeamTavern/Client/Components/Player/ProfileFormInput.purs
@@ -3,11 +3,16 @@ module TeamTavern.Client.Components.Player.ProfileFormInput (Input, Output, Slot
 import Prelude
 
 import Async (Async)
+import Data.Array (foldl)
+import Data.Array as Array
 import Data.Const (Const)
+import Data.Map (Map)
+import Data.Map as Map
 import Data.Maybe (Maybe(..))
 import Data.Variant (SProxy(..))
 import Halogen as H
 import Halogen.HTML as HH
+import Record as Record
 import TeamTavern.Client.Components.Input (inputGroupsHeading, responsiveInputGroups)
 import TeamTavern.Client.Components.Player.ProfileInputGroup (ChildSlots, Field, FieldValue, ambitionsInputGroup, fieldInputGroup, newOrReturningInputGroup)
 
@@ -29,7 +34,7 @@ type Output =
 
 type State =
     { fields :: Array Field
-    , fieldValues :: Array FieldValue
+    , fieldValues :: Map String FieldValue
     , newOrReturning :: Boolean
     , ambitions :: String
     , urlErrors :: Array String
@@ -47,13 +52,20 @@ data Action
 
 type Slot = H.Slot (Const Void) Output Unit
 
+fieldValuesToArray :: forall key value. Map key value -> Array value
+fieldValuesToArray = Array.fromFoldable <<< Map.values
+
+fieldValuesToMap :: forall fields.
+    Array { fieldKey :: String | fields } -> Map String { fieldKey :: String | fields }
+fieldValuesToMap = foldl (\map value -> Map.insert value.fieldKey value map) Map.empty
+
 render :: forall left. State -> H.ComponentHTML Action ChildSlots (Async left)
 render { fields, ambitions, fieldValues, newOrReturning, urlErrors, missingErrors, ambitionsError }
     = HH.div_ $
     [ inputGroupsHeading "Details"
     , responsiveInputGroups $
-        ( fields <#> fieldInputGroup
-            fieldValues UpdateUrl UpdateSingleSelect UpdateMultiSelect urlErrors missingErrors
+        ( fields <#> fieldInputGroup (fieldValuesToArray fieldValues)
+            UpdateUrl UpdateSingleSelect UpdateMultiSelect urlErrors missingErrors
         )
         <>
         [ newOrReturningInputGroup newOrReturning UpdateNewOrReturning ]
@@ -63,33 +75,49 @@ render { fields, ambitions, fieldValues, newOrReturning, urlErrors, missingError
 
 raiseOutput :: forall left. State -> H.HalogenM State Action ChildSlots Output (Async left) Unit
 raiseOutput { fieldValues, newOrReturning, ambitions } =
-    H.raise { fieldValues, newOrReturning, ambitions }
+    H.raise { fieldValues: fieldValuesToArray fieldValues, newOrReturning, ambitions }
 
 handleAction :: forall left. Action -> H.HalogenM State Action ChildSlots Output (Async left) Unit
 handleAction (Receive input) =
-    H.put input
+    H.put (Record.modify (SProxy :: SProxy "fieldValues") fieldValuesToMap input)
 handleAction (UpdateUrl fieldKey url) = do
     state <- H.modify \state -> state
-        { fieldValues = state.fieldValues <#> \fieldValue ->
-            if fieldValue.fieldKey == fieldKey
-            then fieldValue { url = url }
-            else fieldValue
+        { fieldValues =
+            case url of
+            Nothing -> Map.delete fieldKey state.fieldValues
+            Just url' ->
+                Map.insert
+                fieldKey
+                { fieldKey, url: Just url', optionKey: Nothing, optionKeys: Nothing }
+                state.fieldValues
         }
     raiseOutput state
 handleAction (UpdateSingleSelect fieldKey optionKey) = do
     state <- H.modify \state -> state
-        { fieldValues = state.fieldValues <#> \fieldValue ->
-            if fieldValue.fieldKey == fieldKey
-            then fieldValue { optionKey = optionKey }
-            else fieldValue
+        { fieldValues =
+            case optionKey of
+            Nothing -> Map.delete fieldKey state.fieldValues
+            Just optionKey' ->
+                Map.insert
+                fieldKey
+                { fieldKey, url: Nothing, optionKey: Just optionKey', optionKeys: Nothing }
+                state.fieldValues
         }
     raiseOutput state
 handleAction (UpdateMultiSelect fieldKey optionKeys) = do
     state <- H.modify \state -> state
-        { fieldValues = state.fieldValues <#> \fieldValue ->
-            if fieldValue.fieldKey == fieldKey
-            then fieldValue { optionKeys = Just optionKeys }
-            else fieldValue
+        { fieldValues =
+            case Array.uncons optionKeys of
+            Nothing -> Map.delete fieldKey state.fieldValues
+            Just { head, tail } ->
+                Map.insert
+                fieldKey
+                { fieldKey
+                , url: Nothing
+                , optionKey: Nothing
+                , optionKeys: Just $ Array.cons head tail
+                }
+                state.fieldValues
         }
     raiseOutput state
 handleAction (UpdateNewOrReturning newOrReturning) = do
@@ -101,7 +129,7 @@ handleAction (UpdateAmbitions ambitions) = do
 
 component :: forall query left. H.Component HH.HTML query Input Output (Async left)
 component = H.mkComponent
-    { initialState: identity
+    { initialState: Record.modify (SProxy :: SProxy "fieldValues") fieldValuesToMap
     , render
     , eval: H.mkEval $ H.defaultEval
         { handleAction = handleAction
