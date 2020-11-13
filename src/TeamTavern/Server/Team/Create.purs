412ed015ff2d519b59bfe58d8ce07f98461e5003
Branimir Klarić <branimir.klaric.bk@gmail.com>
2020-11-13 21:32:20 +0100

    Factor out generateHandle

--- src/TeamTavern/Server/Team/Create.purs
+++ src/TeamTavern/Server/Team/Create.purs
@@ -4,9 +4,7 @@ import Prelude
 
 import Async (Async, alwaysRight, examineLeftWithEffect)
 import Data.Array as Array
-import Data.Maybe (Maybe(..))
 import Data.Nullable (toNullable)
-import Data.String (CodePoint, codePointFromChar, fromCodePointArray, toCodePointArray)
 import Data.Variant (Variant, match)
 import Effect (Effect)
 import Perun.Request.Body (Body)
@@ -24,59 +22,9 @@ import TeamTavern.Server.Infrastructure.ReadJsonBody (readJsonBody)
 import TeamTavern.Server.Player.Domain.Id (Id)
 import TeamTavern.Server.Player.UpdatePlayer.ValidateTimespan (nullableTimeFrom, nullableTimeTo)
 import TeamTavern.Server.Profile.AddTeamProfile.ValidateAgeSpan (nullableAgeFrom, nullableAgeTo)
+import TeamTavern.Server.Team.Infrastructure.GenerateHandle (Handle, generateHandle)
 import TeamTavern.Server.Team.Infrastructure.LogError (teamHandler)
-import TeamTavern.Server.Team.Infrastructure.ValidateName (Name)
-import TeamTavern.Server.Team.Infrastructure.ValidateName as ValidateName
 import TeamTavern.Server.Team.Infrastructure.ValidateTeam (Team, TeamError, TeamErrors, validateTeam)
-
-newtype Handle = Handle String
-
-aPoint :: CodePoint
-aPoint = codePointFromChar 'a'
-
-aCapPoint :: CodePoint
-aCapPoint = codePointFromChar 'A'
-
-zPoint :: CodePoint
-zPoint = codePointFromChar 'z'
-
-zCapPoint :: CodePoint
-zCapPoint = codePointFromChar 'Z'
-
-onePoint :: CodePoint
-onePoint = codePointFromChar '1'
-
-ninePoint :: CodePoint
-ninePoint = codePointFromChar '9'
-
-dashPoint :: CodePoint
-dashPoint = codePointFromChar '-'
-
-underscorePoint :: CodePoint
-underscorePoint = codePointFromChar '_'
-
-apostrophePoint :: CodePoint
-apostrophePoint = codePointFromChar '\''
-
-isLetter :: CodePoint -> Boolean
-isLetter point = (aPoint <= point && point <= zPoint) || (aCapPoint <= point && point <= zCapPoint)
-
-isNumber :: CodePoint -> Boolean
-isNumber point = onePoint <= point && point <= ninePoint
-
-generateHandle :: Name -> Handle
-generateHandle name =
-    ValidateName.toString name
-    # toCodePointArray
-    <#> (\point ->
-        if isLetter point || isNumber point || point == dashPoint || point == underscorePoint
-        then Just point
-        else if point == apostrophePoint
-        then Nothing
-        else Just underscorePoint)
-    # Array.catMaybes
-    # fromCodePointArray
-    # Handle
 
 queryString :: Query
 queryString = Query """
