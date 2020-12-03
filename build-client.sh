a9c5a1f0bb5c4ed8ad897f48b643470806cfbcc6
Branimir Klarić <branimir.klaric.bk@gmail.com>
2020-12-03 21:35:35 +0100

    Fix build client script

--- build-client.sh
+++ build-client.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 rm -rf dist-client
+mkdir dist-client
 mkdir dist-client/images
 mkdir dist-client/favicons
 mkdir dist-client/css
+mkdir dist-client/webfonts
 cp src/TeamTavern/Client/Static/robots.txt dist-client/robots.txt
 cp src/TeamTavern/Client/Static/index.html dist-client/index.html
 cp src/TeamTavern/Client/Static/Images/* dist-client/images/
 cp src/TeamTavern/Client/Static/Favicons/* dist-client/favicons/
 cp src/TeamTavern/Client/Static/Css/* dist-client/css/
+cp src/TeamTavern/Client/Static/Fonts/* dist-client/webfonts/
 discriminator=`openssl rand -hex 8`
 sassc src/TeamTavern/Client/Style/Main.scss \
     | cleancss -o "dist-client/style.min.${discriminator}.css"
