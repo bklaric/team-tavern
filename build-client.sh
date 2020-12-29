1e8e63cbf9cd0ee4766f5cb068f76bca2a979268
Branimir Klarić <branimir.klaric.bk@gmail.com>
2020-12-29 15:35:36 +0100

    Update tools and client build scripts

--- build-client.sh
+++ build-client.sh
@@ -13,10 +13,10 @@ cp src/TeamTavern/Client/Static/Favicons/* dist-client/favicons/
 cp src/TeamTavern/Client/Static/Css/* dist-client/css/
 cp src/TeamTavern/Client/Static/Fonts/* dist-client/webfonts/
 discriminator=`openssl rand -hex 8`
-sassc src/TeamTavern/Client/Style/Main.scss \
+sass src/TeamTavern/Client/Style/Main.scss \
     | cleancss -o "dist-client/style.min.${discriminator}.css"
 spago bundle-app --main TeamTavern.Client.Main --to dist-client/app.js --no-install --no-build
-uglifyjs --beautify beautify=false,ascii_only=true --mangle --compress -- dist-client/app.js \
-    > "dist-client/app.min.${discriminator}.js"
+uglifyjs dist-client/app.js --beautify beautify=false,ascii_only=true --mangle --compress
+    --output "dist-client/app.min.${discriminator}.js"
 sed -i -e "s/app.min.js/app.min.$discriminator.js/g" dist-client/index.html
 sed -i -e "s/style.min.css/style.min.$discriminator.css/g" dist-client/index.html
