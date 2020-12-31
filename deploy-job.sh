bff8f3243c882fdde1e7f642b6c5003deb7b34a0
Branimir Klarić <branimir.klaric.bk@gmail.com>
2020-12-31 13:39:45 +0100

    Remove deploy-job script

--- deploy-job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-spago bundle-app --main TeamTavern.Job.ConfirmEmailMain --to dist-job/confirm-email-job.js --no-install --no-build
-spago bundle-app --main TeamTavern.Job.CreateProfileMain --to dist-job/create-profile-job.js --no-install --no-build
-spago bundle-app --main TeamTavern.Job.UpdateProfileMain --to dist-job/update-profile-job.js --no-install --no-build
-
-# node dist-job/confirm-email-job.js
-# node dist-job/create-profile-job.js
-# node dist-job/update-profile-job.js
