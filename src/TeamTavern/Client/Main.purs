0d9723889b70fe02dd459ffbd7f6aa3cda1d6f79
Branimir Klarić <branimir.klaric.bk@gmail.com>
2021-03-10 19:26:03 +0100

    Reload ads on orientation change

--- src/TeamTavern/Client/Main.purs
+++ src/TeamTavern/Client/Main.purs
@@ -7,13 +7,16 @@ import Control.Bind (bindFlipped)
 import Data.Maybe (fromJust)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
+import Effect.Class (class MonadEffect)
 import Halogen (hoist, liftEffect)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 import Partial.Unsafe (unsafePartial)
 import TeamTavern.Client.Router (Query(..), router)
+import TeamTavern.Client.Script.ReloadAds (reloadAds)
 import Web.DOM.NonElementParentNode (getElementById)
-import Web.Event.EventTarget (addEventListener)
+import Web.Event.Event (Event, EventType(..))
+import Web.Event.EventTarget (EventListener, addEventListener)
 import Web.Event.EventTarget as DOM
 import Web.HTML (window)
 import Web.HTML.Event.PopStateEvent as PSE
@@ -25,6 +28,13 @@ import Web.HTML.Location as Location
 import Web.HTML.Window (document)
 import Web.HTML.Window as Window
 
+createListener :: forall monad. MonadEffect monad => (Event -> Effect Unit) -> monad EventListener
+createListener = liftEffect <<< DOM.eventListener
+
+addWindowListener :: forall monad. MonadEffect monad => EventType -> EventListener -> monad Unit
+addWindowListener event listener =
+    window <#> Window.toEventTarget >>= addEventListener event listener false # liftEffect
+
 main :: Effect Unit
 main = HA.runHalogenAff do
     body <- HA.awaitBody
@@ -32,11 +42,10 @@ main = HA.runHalogenAff do
     state <- window >>= Window.history >>= History.state # liftEffect
     path <- window >>= Window.location >>= Location.pathname # liftEffect
     { query } <- runUI (hoist (asyncToAff absurd) (router state path)) unit spa
-    listener <- liftEffect $ DOM.eventListener \event -> do
+    navigationListener <- createListener \event -> do
         let state' = PSE.fromEvent event # unsafePartial fromJust # PSE.state
         path' <- window >>= Window.location >>= Location.pathname
         query (ChangeRoute state' path' unit) # launchAff_
-    window
-        <#> Window.toEventTarget
-        >>= addEventListener PSET.popstate listener false
-        # liftEffect
+    addWindowListener PSET.popstate navigationListener
+    orientationListener <- createListener $ const reloadAds
+    addWindowListener (EventType "orientationchange") orientationListener
