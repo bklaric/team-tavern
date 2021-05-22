5e470798c7141c1335f4d262663f3b7190d63e6b
Branimir Klarić <branimir.klaric.bk@gmail.com>
2021-05-22 12:17:17 +0200

    Fix email validation

--- src/TeamTavern/Server/Infrastructure/ValidateEmail.purs
+++ src/TeamTavern/Server/Infrastructure/ValidateEmail.purs
@@ -6,6 +6,7 @@ import Async (Async)
 import Async.Validated (fromValidated)
 import Data.Bifunctor.Label (labelMap)
 import Data.Either (fromRight)
+import Data.List.Types (NonEmptyList)
 import Data.Maybe (isJust)
 import Data.String (trim)
 import Data.String.Regex (Regex, match, regex)
@@ -14,8 +15,7 @@ import Data.Validated.Label (ValidatedNelVari)
 import Data.Validated.Label as Validated
 import Data.Variant (SProxy(..), Variant)
 import Partial.Unsafe (unsafePartial)
-import Unsafe.Coerce (unsafeCoerce)
-import Wrapped.String (invalid, tooLong)
+import Wrapped.String (TooLong, Invalid, invalid, tooLong)
 import Wrapped.Validated as Wrapped
 
 newtype Email = Email String
@@ -23,15 +23,14 @@ newtype Email = Email String
 emailRegex :: Regex
 emailRegex = regex """^[^\s@]+@[^\s@]+\.[^\s@]+$""" unicode # unsafePartial fromRight
 
-closeRow :: forall carrier rowTaking row. carrier (rowTaking row) -> carrier (rowTaking ())
-closeRow = unsafeCoerce
+type EmailError = Variant (invalid :: Invalid, tooLong :: TooLong )
 
 validateEmail :: forall errors. String -> ValidatedNelVari (email :: Array String | errors) Email
 validateEmail email =
     Wrapped.create trim [invalid (match emailRegex >>> isJust), tooLong 254] Email email
     # Validated.labelMap (SProxy :: _ "email") \errors ->
         [ "Error validating email: " <> email
-        , "Failed with following errors: " <> show (closeRow errors)
+        , "Failed with following errors: " <> show (errors :: NonEmptyList EmailError)
         ]
 
 validateEmail' :: forall errors. String -> Async (Variant (email :: Array String | errors)) Email
@@ -40,7 +39,7 @@ validateEmail' email =
     # fromValidated
     # labelMap (SProxy :: _ "email") \errors ->
         [ "Error validating email: " <> email
-        , "Failed with following errors: " <> show (closeRow errors)
+        , "Failed with following errors: " <> show (errors :: NonEmptyList EmailError)
         ]
 
 toString :: Email -> String
