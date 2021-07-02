bbf0b6586924eba0993e89a00c33f129abfb8f80
Branimir Klarić <branimir.klaric.bk@gmail.com>
2021-07-02 23:07:31 +0200

    Use register component on register page

--- src/TeamTavern/Client/Pages/Register.purs
+++ src/TeamTavern/Client/Pages/Register.purs
@@ -17,15 +17,16 @@ import Data.Variant (match)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
-import Halogen.HTML.Properties (ButtonType(..), InputType(..))
 import Halogen.HTML.Properties as HP
 import Simple.JSON as Json
 import Simple.JSON.Async as JsonAsync
+import TeamTavern.Client.Components.RegistrationInput (registrationInput)
+import TeamTavern.Client.Components.RegistrationInput as RegistrationInput
 import TeamTavern.Client.Pages.Onboarding as Onboarding
 import TeamTavern.Client.Script.Cookie (hasPlayerIdCookie)
 import TeamTavern.Client.Script.Meta (setMeta)
 import TeamTavern.Client.Script.Navigate (navigate, navigateReplace_, navigateWithEvent_)
-import TeamTavern.Client.Snippets.ErrorClasses (inputErrorClass, otherErrorClass)
+import TeamTavern.Client.Snippets.ErrorClasses (otherErrorClass)
 import TeamTavern.Server.Player.Register.ReadDto (RegisterDto)
 import TeamTavern.Server.Player.Register.SendResponse as Register
 import Web.Event.Event (preventDefault)
@@ -34,36 +35,22 @@ import Web.UIEvent.MouseEvent (MouseEvent)
 
 data Action
     = Initialize
-    | UpdateNickname String
-    | UpdatePassword String
-    | TogglePasswordVisibility
+    | UpdateRegistration RegistrationInput.Output
     | Register Event
     | Navigate String MouseEvent
 
 type State =
-    { nickname :: String
-    , password :: String
-    , passwordShown :: Boolean
-    , nicknameError :: Boolean
-    , passwordError :: Boolean
-    , nicknameTaken :: Boolean
+    { registration :: RegistrationInput.Input
     , otherError :: Boolean
     , submitting :: Boolean
     }
 
 type Slot = H.Slot (Const Void) Void
 
-render :: forall left slots. State -> H.ComponentHTML Action slots (Async left)
-render
-    { nickname
-    , password
-    , passwordShown
-    , nicknameError
-    , passwordError
-    , nicknameTaken
-    , otherError
-    , submitting
-    } = HH.form
+render :: forall left.
+    State -> H.ComponentHTML Action (registrationInput :: RegistrationInput.Slot) (Async left)
+render { registration, otherError, submitting } =
+    HH.form
     [ HP.class_ $ HH.ClassName "form"
     , HE.onSubmit $ Just <<< Register
     ]
@@ -76,65 +63,10 @@ render
             [ HH.text "TeamTavern" ]
         , HH.text " account"
         ]
-    , HH.div [ HP.class_ $ HH.ClassName "input-group" ]
-        [ HH.label
-            [ HP.class_ $ HH.ClassName "input-label", HP.for "nickname" ]
-            [ HH.text "Nickname" ]
-        , HH.input
-            [ HP.id_ "nickname"
-            , HP.class_ $ HH.ClassName "text-line-input"
-            , HE.onValueInput $ Just <<< UpdateNickname
-            ]
-        , HH.p
-            [ HP.class_ $ inputErrorClass nicknameError ]
-            [ HH.text
-                $ "Nickname cannot be more than 40 characters long "
-                <> "and can only contain alphanumeric characters, dashes, underscores and dots." ]
-        , HH.p
-            [ HP.class_ $ inputErrorClass nicknameTaken ]
-            [ HH.text
-                "This nickname is already taken, please pick another one." ]
-        ]
-    , HH.div [ HP.class_ $ HH.ClassName "input-group" ]
-        [ HH.label
-            [ HP.class_ $ HH.ClassName "input-label", HP.for "password" ]
-            [ HH.text "Password" ]
-        , HH.div [ HP.class_ $ HH.ClassName "password-input-container" ]
-            [ HH.input
-                [ HP.id_ "password"
-                , HP.class_ $ HH.ClassName "password-input"
-                , HP.type_
-                    if passwordShown
-                    then InputText
-                    else InputPassword
-                , HE.onValueInput $ Just <<< UpdatePassword
-                ]
-            , HH.button
-                [ HP.class_ $ HH.ClassName "password-input-button"
-                , HP.type_ ButtonButton
-                , HP.title
-                    if passwordShown
-                    then "Hide password"
-                    else "Show password"
-                , HE.onClick $ Just <<< const TogglePasswordVisibility
-                ]
-                [ HH.i
-                    [ HP.class_ $ HH.ClassName
-                        if passwordShown
-                        then "fas fa-eye-slash"
-                        else "fas fa-eye"
-                    ]
-                    []
-                ]
-            ]
-        , HH.p
-            [ HP.class_ $ inputErrorClass passwordError ]
-            [ HH.text $ "Password must have at least 8 characters."
-            ]
-        ]
+    , registrationInput registration UpdateRegistration
     , HH.button
         [ HP.class_ $ HH.ClassName "form-submit-button"
-        , HP.disabled $ nickname == "" || password == "" || submitting
+        , HP.disabled $ registration.nickname == "" || registration.password == "" || submitting
         ]
         [ HH.i [ HP.class_ $ HH.ClassName "fas fa-user-check button-icon" ] []
         , HH.text $
@@ -157,7 +89,7 @@ render
     ]
 
 sendRegisterRequest :: forall left. State -> Async left (Maybe State)
-sendRegisterRequest state @ { nickname, password } = Async.unify do
+sendRegisterRequest state @ { registration: { nickname, password } } = Async.unify do
     response <- Fetch.fetch "/api/players"
         (  Fetch.method := POST
         <> Fetch.body := Json.writeJSON ({ nickname, password } :: RegisterDto)
@@ -172,13 +104,13 @@ sendRegisterRequest state @ { nickname, password } = Async.unify do
                 (\(error :: Register.BadRequestContent) -> Just $ match
                     { registration: foldl (\state' -> match
                         { invalidNickname:
-                            const $ state' { nicknameError = true }
+                            const $ state' { registration { nicknameError = true } }
                         , invalidPassword:
-                            const $ state' { passwordError = true }
+                            const $ state' { registration { passwordError = true } }
                         })
                         state
                     , nicknameTaken:
-                        const $ state { nicknameTaken = true }
+                        const $ state { registration { nicknameTaken = true } }
                     }
                     error)
         _ -> pure $ Just $ state { otherError = true }
@@ -189,18 +121,21 @@ handleAction :: forall slots output left.
 handleAction Initialize = do
     H.liftEffect $ whenM hasPlayerIdCookie $ navigateReplace_ "/"
     setMeta "Create account | TeamTavern" "Create your TeamTavern account."
-handleAction (UpdateNickname nickname) =
-    H.modify_ (_ { nickname = nickname })
-handleAction (UpdatePassword password) =
-    H.modify_ (_ { password = password })
-handleAction TogglePasswordVisibility =
-    H.modify_ (\state -> state { passwordShown = not state.passwordShown })
+handleAction (UpdateRegistration registration) =
+    H.modify_ _
+        { registration
+            { nickname = registration.nickname
+            , password = registration.password
+            }
+        }
 handleAction (Register event) = do
     H.liftEffect $ preventDefault event
     state <- H.gets (_
-        { nicknameError  = false
-        , passwordError  = false
-        , nicknameTaken  = false
+        { registration
+            { nicknameError  = false
+            , passwordError  = false
+            , nicknameTaken  = false
+            }
         , otherError     = false
         , submitting     = true
         })
@@ -215,12 +150,7 @@ handleAction (Navigate url event) =
 component :: forall query input output left. H.Component HH.HTML query input output (Async left)
 component = H.mkComponent
     { initialState: const
-        { nickname: ""
-        , password: ""
-        , passwordShown: false
-        , nicknameError: false
-        , passwordError: false
-        , nicknameTaken: false
+        { registration: RegistrationInput.emptyInput
         , otherError: false
         , submitting: false
         }
