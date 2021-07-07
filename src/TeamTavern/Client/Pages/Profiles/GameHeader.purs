72848afc7f53204f92c5823ff892caa6918e8a1f
Branimir Klarić <branimir.klaric.bk@gmail.com>
2021-07-07 16:06:22 +0200

    Add D2DCL banner

--- src/TeamTavern/Client/Pages/Profiles/GameHeader.purs
+++ src/TeamTavern/Client/Pages/Profiles/GameHeader.purs
@@ -2,7 +2,9 @@ module TeamTavern.Client.Pages.Profiles.GameHeader (ProfileTab(..), Tab(..), Inp
 
 import Prelude
 
+import Data.Array as Array
 import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
 import TeamTavern.Client.Components.Ads (descriptionLeaderboard)
 import TeamTavern.Client.Components.Content (contentDescription, contentHeader, contentHeaderSection, contentHeading')
 import TeamTavern.Client.Snippets.Class as HS
@@ -14,7 +16,7 @@ data Tab = Profiles ProfileTab | Competitions
 type Input = { title :: String, shortTitle :: String, tab :: Tab }
 
 gameHeader :: forall slots action. Input -> HH.HTML slots action
-gameHeader { title, shortTitle, tab } = HH.div_
+gameHeader { title, shortTitle, tab } = HH.div_ $
     [ contentHeader
         [ contentHeaderSection
             [ contentHeading'
@@ -29,5 +31,10 @@ gameHeader { title, shortTitle, tab } = HH.div_
         Profiles Players -> "Find " <> shortTitle <> " players looking for a team. Create your own player profile and let everyone know you're looking to team up."
         Profiles Teams -> "Find  " <> shortTitle <> " teams looking for players. Create your own team profile and recruit new members for your team."
         Competitions -> "Apply for open " <> shortTitle <> " leagues and tournaments and compete for prizes and boasting rights."
-    , descriptionLeaderboard
     ]
+    <>
+    if shortTitle == "Dota 2"
+    then Array.singleton $
+        HH.a [ HP.href "https://discord.gg/d2zjFWxC8M", HP.target "_blank", HP.rel "noopener", HP.title "Dota 2 Dream League Community" ]
+        [ HH.img [ HS.class_ "partner-banner", HP.src $ "https://cdn.discordapp.com/attachments/861682299107278868/861682469459722280/dota2_logo_banner_2.png" ] ]
+    else [ descriptionLeaderboard ]
