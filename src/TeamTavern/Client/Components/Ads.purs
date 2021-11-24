35de16eb3d40ae38afcff31c1fc2ff83738a62e4
Branimir Klarić <branimir.klaric.bk@gmail.com>
2021-11-24 20:15:53 +0100

    Update ads

--- src/TeamTavern/Client/Components/Ads.purs
+++ src/TeamTavern/Client/Components/Ads.purs
@@ -7,10 +7,10 @@ import TeamTavern.Client.Snippets.Class as HS
 -- Desktop
 
 descriptionLeaderboard :: forall slots action. HH.HTML slots action
-descriptionLeaderboard = HH.div [ HP.id_ "nn_lb1", HS.class_ "description-leaderboard" ] []
+descriptionLeaderboard = HH.div [ HP.id_ "nn_lb2", HS.class_ "description-leaderboard" ] []
 
 stickyLeaderboard :: forall slots action. HH.HTML slots action
-stickyLeaderboard = HH.div [ HP.id_ "nn_lb2", HS.class_ "sticky-leaderboard" ] []
+stickyLeaderboard = HH.div [ HP.id_ "nn_lb1", HS.class_ "sticky-leaderboard" ] []
 
 filtersMpu :: forall slots action. HH.HTML slots action
 filtersMpu = HH.div [ HP.id_ "nn_mpu1", HS.class_ "filters-mpu" ] []
@@ -18,10 +18,10 @@ filtersMpu = HH.div [ HP.id_ "nn_mpu1", HS.class_ "filters-mpu" ] []
 -- Mobile
 
 mobileDescriptionLeaderboard :: forall slots action. HH.HTML slots action
-mobileDescriptionLeaderboard = HH.div [ HP.id_ "nn_mobile_lb1_sticky", HS.class_ "description-leaderboard" ] []
+mobileDescriptionLeaderboard = HH.div [ HP.id_ "nn_mobile_lb2", HS.class_ "description-leaderboard" ] []
 
 mobileStickyLeaderboard :: forall slots action. HH.HTML slots action
-mobileStickyLeaderboard = HH.div [ HP.id_ "nn_mobile_lb2", HS.class_ "sticky-leaderboard" ] []
+mobileStickyLeaderboard = HH.div [ HP.id_ "nn_mobile_lb1_sticky", HS.class_ "sticky-leaderboard" ] []
 
 -- Both
 
