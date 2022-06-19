6ba5f06794eec3aaee8facce1a3d79b21f45648d
Branimir Klarić <branimir.klaric.bk@gmail.com>
2022-06-19 18:36:02 +0200

    Remove uglifyjs since esbuild can minify

--- build-client.sh
+++ build-client.sh
@@ -18,11 +18,8 @@ cp src/TeamTavern/Client/Static/Fonts/* dist-client/webfonts/
 discriminator=`openssl rand -hex 8`
 sass src/TeamTavern/Client/Style/Main.scss \
     | cleancss -o "dist-client/style.min.${discriminator}.css"
-spago bundle-app --main TeamTavern.Client.Main --to dist-client/app.js --no-install --no-build
-uglifyjs dist-client/app.js --beautify beautify=false,ascii_only=true --mangle --compress \
-    --output "dist-client/app.min.${discriminator}.js"
+spago bundle-app --main TeamTavern.Client.Main --to dist-client/app.min.${discriminator}.js --no-install --no-build
 sed -i -e "s/app.min.js/app.min.$discriminator.js/g" dist-client/index.html
 sed -i -e "s/style.min.css/style.min.$discriminator.css/g" dist-client/index.html
 sed -i -e "s/app.min.js/app.min.$discriminator.js/g" dist-client/index.prerender.html
 sed -i -e "s/style.min.css/style.min.$discriminator.css/g" dist-client/index.prerender.html
-rm dist-client/app.js
--- install-tools.sh
+++ install-tools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-npm install -g purescript spago sass clean-css-cli uglify-js
+npm install -g purescript spago sass clean-css-cli
 
 # /home/bklaric/.nvm/versions/node/v18.3.0/lib
 # ├── clean-css-cli@5.6.0
@@ -7,5 +7,4 @@ npm install -g purescript spago sass clean-css-cli uglify-js
 # ├── npm@8.12.1
 # ├── purescript@0.15.2
 # ├── sass@1.52.3
-# ├── spago@0.20.9
-# └── uglify-js@3.16.0
+# └── spago@0.20.9
