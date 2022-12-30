d2346dd3afc1cd7beddd1210361f2be0488e6af3
Branimir Klarić <branimir.klaric.bk@gmail.com>
2022-12-30 15:24:16 +0100

    Refactor modal component to use hooks

--- src/TeamTavern/Client/Components/Modal.purs
+++ src/TeamTavern/Client/Components/Modal.purs
@@ -4,29 +4,19 @@ import Prelude
 
 import Data.Const (Const)
 import Data.Maybe (Maybe(..))
-import Type.Proxy (Proxy(..))
 import Effect.Class (class MonadEffect)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
+import Halogen.Hooks as Hooks
 import TeamTavern.Client.Script.Unscrollable (makeWindowScrollable, makeWindowUnscrollable)
 import TeamTavern.Client.Snippets.Class as HS
+import Type.Proxy (Proxy(..))
 import Unsafe.Reference (unsafeRefEq)
-import Web.Event.Event (target)
-import Web.HTML.HTMLElement (fromEventTarget)
-import Web.UIEvent.MouseEvent (MouseEvent, toEvent)
-
-type Input (input :: Type) = input
-
-type State (input :: Type) = Input input
-
-data Action output
-    = Init
-    | Finalize
-    | BackgroundClose MouseEvent
-    | ButtonClose
-    | OutputRaise output
+import Web.Event.Event as Event
+import Web.HTML.HTMLElement as HtmlElement
+import Web.UIEvent.MouseEvent as MouseEvent
 
 data Output output = CloseClicked | OutputRaised output
 
@@ -34,59 +24,39 @@ type ChildSlots query output = (content :: H.Slot query output Unit)
 
 type Slot output = H.Slot (Const Void) output Unit
 
-render
-    :: ∀ query input output monad
-    .  String
-    -> H.Component query input output monad
-    -> State input
-    -> HH.ComponentHTML (Action output) (ChildSlots query output) monad
-render title content input =
-    HH.div
-    [ HP.class_ $ H.ClassName "modal-background"
-    , HP.ref $ H.RefLabel "modal-background"
-    , HE.onClick BackgroundClose
-    ]
-    [ HH.div [ HS.class_ "modal-content" ]
-        [ HH.h1 [ HS.class_ "modal-title" ]
-            [ HH.text title
-            , HH.button
-                [ HS.class_ "modal-close-button"
-                , HE.onClick $ const ButtonClose
-                ]
-                [ HH.i [ HS.class_ "fas fa-times modal-close-button-icon" ] [] ]
-            ]
-        , HH.slot (Proxy :: _ "content") unit content input OutputRaise
-        ]
-    ]
-
-handleAction :: ∀ state action slots output monad. MonadEffect monad =>
-    Action output -> H.HalogenM state action slots (Output output) monad Unit
-handleAction Init =
-    makeWindowUnscrollable
-handleAction Finalize =
-    makeWindowScrollable
-handleAction (BackgroundClose event) = do
-    background <- H.getHTMLElementRef (H.RefLabel "modal-background")
-    eventTarget <- event # toEvent # target >>= fromEventTarget # pure
-    case background, eventTarget of
-        Just background', Just eventTarget'
-            | unsafeRefEq background' eventTarget' -> H.raise CloseClicked
-        _, _ -> pure unit
-handleAction ButtonClose = H.raise CloseClicked
-handleAction (OutputRaise message) = H.raise $ OutputRaised message
-
 component
-    :: ∀ query input output monad
+    :: ∀ query' monad output query input
     .  MonadEffect monad
     => String
     -> H.Component query input output monad
-    -> H.Component query input (Output output) monad
-component title content = H.mkComponent
-    { initialState: identity
-    , render: render title content
-    , eval: H.mkEval $ H.defaultEval
-        { handleAction = handleAction
-        , initialize = Just Init
-        , finalize = Just Finalize
-        }
-    }
+    -> H.Component query' input (Output output) monad
+component title content = Hooks.component \{ outputToken } input -> Hooks.do
+    Hooks.useLifecycleEffect do
+        makeWindowUnscrollable
+        pure $ Just makeWindowScrollable
+    let onBackgroundClick event = do
+            background <- Hooks.getHTMLElementRef (H.RefLabel "modal-background")
+            let eventTarget = event # MouseEvent.toEvent # Event.target >>= HtmlElement.fromEventTarget
+            case background, eventTarget of
+                Just background', Just eventTarget'
+                    | unsafeRefEq background' eventTarget' -> Hooks.raise outputToken CloseClicked
+                _, _ -> pure unit
+    let onCloseClick = Hooks.raise outputToken CloseClicked
+    let onRaiseOutput message = Hooks.raise outputToken $ OutputRaised message
+    Hooks.pure $ HH.div
+        [ HP.class_ $ H.ClassName "modal-background"
+        , HP.ref $ H.RefLabel "modal-background"
+        , HE.onClick onBackgroundClick
+        ]
+        [ HH.div [ HS.class_ "modal-content" ]
+            [ HH.h1 [ HS.class_ "modal-title" ]
+                [ HH.text title
+                , HH.button
+                    [ HS.class_ "modal-close-button"
+                    , HE.onClick $ const onCloseClick
+                    ]
+                    [ HH.i [ HS.class_ "fas fa-times modal-close-button-icon" ] [] ]
+                ]
+            , HH.slot (Proxy :: _ "content") unit content input onRaiseOutput
+            ]
+        ]
