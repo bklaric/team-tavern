87f7eee0c841c1b26e5ef75bf3849d3f27b8af5a
Branimir Klarić <branimir.klaric.bk@gmail.com>
2022-12-30 22:42:51 +0100

    Log request for 404 on routing instead of error record

--- src/Jarilo/Serve.purs
+++ src/Jarilo/Serve.purs
@@ -2,21 +2,35 @@ module Jarilo.Serve where
 
 import Prelude
 
-import Async (unify)
+import Async (examineLeftWithEffect, unify)
 import Data.Bifunctor (lmap)
+import Data.Either (Either)
+import Data.HTTP.Method (CustomMethod, Method)
+import Data.List (List)
+import Data.Map (Map)
 import Effect (Effect)
-import Effect.Console (log)
-import Effect.Unsafe (unsafePerformEffect)
 import Jarilo.Router.Junction (class JunctionRouter, junctionRouter)
 import Node.Server (ListenOptions)
 import Perun.Async.Server (run_)
+import Perun.Request (Request)
+import Record.Extra (pick)
+import TeamTavern.Server.Infrastructure.Log (logStamped)
 import Type.Proxy (Proxy)
-import Unsafe.Coerce (unsafeCoerce)
+import URI.Extra.QueryPairs (Key, QueryPairs, Value)
+import URI.Path.Segment (PathSegment)
+
+type ShowableRequest =
+    { method :: Either CustomMethod Method
+    , path :: List PathSegment
+    , query :: QueryPairs Key Value
+    , cookies :: Map String String
+    }
 
 serve :: ∀ errors handlers junction. JunctionRouter junction errors handlers =>
     Proxy junction -> ListenOptions -> Record handlers -> Effect Unit
 serve proxy options handlers =
     run_ options \request ->
         junctionRouter proxy handlers request
-        # lmap (unsafeCoerce >>> log >>> unsafePerformEffect >>> const ({ statusCode: 404, headers: mempty, body: mempty }))
+        # examineLeftWithEffect (const $ logStamped $ show $ (pick :: Request -> ShowableRequest) request)
+        # lmap (const { statusCode: 404, headers: mempty, body: mempty })
         # unify
