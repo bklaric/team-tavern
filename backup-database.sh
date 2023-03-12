bb2c05c225d3315ddb4c2e2f18a221645f4a68ac
Branimir Klarić <branimir.klaric.bk@gmail.com>
2023-03-13 00:18:05 +0100

    Don't store old backups or temp files

--- backup-database.sh
+++ backup-database.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 # UTC ISO timestamp for file name.
 DATETIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-# Dump database and save the file to disk.
-docker exec postgres pg_dump -f /backups/$DATETIME-database-backup.sql
-# Encode database backup in base64 and store it in variable.
-BACKUP_BASE64=$(cat $POSTGRES_BACKUP_PATH/$DATETIME-database-backup.sql | base64)
-# Prepare JSON for POST body to Sendgrid API.
+# Dump database, encode it in base64 and store it in variable.
+BACKUP_BASE64=$(docker exec postgres pg_dump | base64)
+# Prepare JSON body for Sendgrid API.
 DATA='{"personalizations": [{"to": [{"email": "branimir.klaric.bk@gmail.com"}]}],"from": {"email": "backup@teamtavern.net"},"subject":"Database backup '$DATETIME'","content": [{"type": "text/plain","value": "Database backup."}], "attachments": [{"content": "'$BACKUP_BASE64'", "type": "application/sql", "filename": "'$DATETIME'-database-backup.sql"}]}'
-# Create a temp file for JSON body.
-TEMP=$(mktemp database-backup-temp-XXXXXXXXX)
-# Write JSON body into temp file.
+# Write JSON body into a file.
 # We're doing this because curl complains if we pass it a large --data argument.
 # However, it can read --data from files just fine.
-echo $DATA > $TEMP
+echo $DATA > ~/database-backup-body
 # Call the Sendgrid API.
 curl --request POST \
     --url https://api.sendgrid.com/v3/mail/send \
     --header "authorization: Bearer $SENDGRID_API_KEY" \
     --header 'Content-Type: application/json' \
-    --data @$TEMP
+    --data @${HOME}/database-backup-body
