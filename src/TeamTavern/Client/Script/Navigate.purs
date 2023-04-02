9a34a4f562f7cfa16f02debfea5c9f7a951ca6bb
Branimir Klarić <branimir.klaric.bk@gmail.com>
2023-04-02 14:51:22 +0200

    Fix staging navigation so preboarding works

--- src/TeamTavern/Client/Script/Navigate.purs
+++ src/TeamTavern/Client/Script/Navigate.purs
@@ -11,6 +11,7 @@ module TeamTavern.Client.Script.Navigate
 
 import Prelude
 
+import Data.String (Pattern(..), split)
 import Effect.Class (class MonadEffect, liftEffect)
 import Effect.Timer (setTimeout)
 import TeamTavern.Client.Script.PopStateEvent as PopStateEvent
@@ -20,18 +21,29 @@ import Web.HTML (window)
 import Web.HTML as Html
 import Web.HTML.History (DocumentTitle(..), URL(..), pushState)
 import Web.HTML.History as History
-import Web.HTML.Location (setHref)
+import Web.HTML.Location (hostname, setHref)
 import Web.HTML.Window (history, location)
 import Web.HTML.Window as Window
 import Web.UIEvent.MouseEvent (MouseEvent)
 import Yoga.JSON (class WriteForeign, write)
 
+pathAndQueryStaging :: ∀ effect. MonadEffect effect => String -> effect String
+pathAndQueryStaging pathAndQuery = liftEffect do
+    hostname <- window >>= location >>= hostname
+    let pathAndQuerySplit = split (Pattern "?") pathAndQuery
+    let stagingOrLocalhost = hostname == "staging.teamtavern.net" || hostname == "localhost"
+    pure case pathAndQuerySplit, stagingOrLocalhost of
+        [path], true -> path <> "?nntestads=staging"
+        [path, query], true -> path <> "?" <> query <> "&nntestads=staging"
+        _, _ -> pathAndQuery
+
 navigate :: ∀ effect state. MonadEffect effect =>
     WriteForeign state => state -> String -> effect Unit
 navigate state path = liftEffect do
+    path' <- pathAndQueryStaging path
     window
         >>= history
-        >>= pushState (write state) (DocumentTitle path) (URL path)
+        >>= pushState (write state) (DocumentTitle path') (URL path')
     popStateEvent <-
         PopStateEvent.create (write state) <#> PopStateEvent.toEvent
     void $ setTimeout 0 do
@@ -61,7 +73,8 @@ replaceState state path  =
 navigateReplace :: ∀ state effect. MonadEffect effect => WriteForeign state =>
     state -> String -> effect Unit
 navigateReplace state path = liftEffect do
-    replaceState state path
+    path' <- pathAndQueryStaging path
+    replaceState state path'
     popStateEvent <-
         PopStateEvent.create (write state) <#> PopStateEvent.toEvent
     void $ setTimeout 0
