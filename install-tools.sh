4e9b92ec19cd121188c31fa45503cbf0a52bc505
Branimir Klarić <branimir.klaric@gideonbros.ai>
2023-11-16 08:41:36 +0100

    Use volta instead of nvm

--- install-tools.sh
+++ install-tools.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 npm install -g purescript spago sass esbuild
-# ❯ npm ls -g
-# /home/bklaric/.nvm/versions/node/v18.16.0/lib
-# ├── corepack@0.17.0
-# ├── esbuild@0.19.4
-# ├── npm@9.5.1
-# ├── purescript@0.15.11
-# ├── sass@1.68.0
-# └── spago@0.21.0
