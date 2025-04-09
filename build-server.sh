9c557b337850a5ddc80bfbab42755dfe8380a328
Branimir Klarić <branimir.klaric.bk@gmail.com>
2025-04-09 18:29:36 +0200

    Copy package.json to dist-server so the container uses a separate node_modules

--- build-server.sh
+++ build-server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# spago bundle-app --main TeamTavern.Server.Main --to dist-server/server.js --no-install --no-build --platform node
+cp package.json dist-server/package.json
+cp package-lock.json dist-server/package-lock.json
 esbuild output/TeamTavern.Server.Main/index.js --outfile=dist-server/server.js --platform=node --bundle --format=cjs --external:bcrypt --external:pg --external:@sendgrid/mail
 echo "main();" >> dist-server/server.js
